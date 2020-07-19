bd7ba64a353aa0593f2324fd88237d85bfbeefd2
Colin McCabe <colin@localhost>
2020-07-19 15:51:47 -0400

    Add commands and command infrastructure

---
+++ build-commands.fish
@@ -0,0 +1,19 @@
+#!/usr/bin/env fish
+
+set base_images alpine:3.12 debian:buster-slim
+
+for img in $base_images
+  docker pull $img
+end
+
+for cmd in (cat commands)
+  echo "Building $cmd"
+  pushd $cmd
+  docker build -t $cmd .
+  popd
+end
+
+for cmd in (cat commands)
+  echo "Linking $cmd"
+  ln -sf (pwd)/$cmd/run ~/bin/$cmd
+end
